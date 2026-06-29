cd586c2275417c7d5f2ec82464b82415a1af897e
Vanshika-OFFICIAL <vanshika9354@gmail.com>
2026-06-29 15:42:00 +0530

    updated redirecting page

--- frontend/src/app/dashboard/layout.js
+++ frontend/src/app/dashboard/layout.js
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 import Sidebar from "@/components/dashboard/Sidebar";
 import Navbar from "@/components/common/Navbar";
@@ -10,6 +11,27 @@ export default function DashboardLayout({
 }) {
   const [sidebarOpen, setSidebarOpen] =
     useState(false);
+
+  const [loading, setLoading] =
+    useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const token =
+      localStorage.getItem("token");
+
+    if (!token) {
+      router.replace("/login");
+      return;
+    }
+
+    setLoading(false);
+  }, [router]);
+
+  if (loading) {
+    return null;
+  }
 
   return (
     <div
@@ -26,45 +48,47 @@ export default function DashboardLayout({
           setSidebarOpen(false)
         }
       />
-{sidebarOpen && (
-  <div
-    onClick={() =>
-      setSidebarOpen(false)
-    }
-    className="
-      fixed
-      inset-0
-      bg-black/50
-      z-40
-      lg:hidden
-    "
-  />
-)}
+
+      {sidebarOpen && (
+        <div
+          onClick={() =>
+            setSidebarOpen(false)
+          }
+          className="
+            fixed
+            inset-0
+            bg-black/50
+            z-40
+            lg:hidden
+          "
+        />
+      )}
+
       <div
         className="
         flex-1
         "
       >
-       <Navbar
-  openSidebar={() =>
-    setSidebarOpen(true)
-  }
-/>
+        <Navbar
+          openSidebar={() =>
+            setSidebarOpen(true)
+          }
+        />
 
-<main
-  className="
-    pt-20
+        <main
+          className="
+            pt-20
 
-    px-4
-    pb-4
+            px-4
+            pb-4
 
-    md:px-6
-    md:pb-6
+            md:px-6
+            md:pb-6
 
-    lg:pt-8
-    lg:p-8
-  "
->
+            lg:pt-8
+            lg:p-8
+          "
+        >
           {children}
         </main>
       </div>
